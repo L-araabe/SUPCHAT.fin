14e5392fe4583ad2cc76657481be08cc7f1c477b
AJOUIRJA Ayoub <91870717+AJOUIRJAayoub@users.noreply.github.com>
2025-06-11 04:11:47 +0200

    fix channel message route precedence

--- supchat-backend-main/routes/messages.routes.js
+++ supchat-backend-main/routes/messages.routes.js
@@ -4,11 +4,11 @@ const messageCtrl = require("../controllers/mesages.controller");
 const { protect } = require("../middleware/auth");
 
 router.post("/", protect, messageCtrl.sendMessage);
-router.get("/:chatId", protect, messageCtrl.getChatMessages);
 router.get("/channel/:channelId", protect, messageCtrl.getChannelMessages);
+router.get("/unread/channel/:channelId", protect, messageCtrl.unReadMessagesOfChannel);
+router.get("/:chatId", protect, messageCtrl.getChatMessages);
 router.put("/seen", protect, messageCtrl.markAsSeen);
 router.delete("/:id", protect, messageCtrl.deleteMessage);
 router.get("/unread/:chatId", protect, messageCtrl.unReadMessagesOfChat);
-router.get("/unread/channel/:channelId", protect, messageCtrl.unReadMessagesOfChannel);
 
 module.exports = router;
